7e552ccc4fbf19111445db5905683a6b65a80bb7
Yin <yinnnn17@gmail.com>
2021-01-12 15:04:15 -0800

    add filter feature

--- src/components/PlanList.js
+++ src/components/PlanList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container, CardGroup } from 'react-bootstrap';
+import { Container, Form } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext';
 import firebase from '../firebase';
 import Plan from './Plan';
@@ -7,12 +7,12 @@ import './PlanList.css';
 export default function PlanList() {
 	const { currentUser } = useAuth();
 	const [planList, setPlanList] = useState([]);
+	const [query, setQuery] = useState('');
 	useEffect(() => {
 		const uid = currentUser.uid;
 		const planRef = firebase.database().ref('visionboard/' + uid);
 		planRef.on('value', (snapshot) => {
 			const plans = snapshot.val();
-			console.log(plans);
 			const tempList = [];
 			for (let id in plans) {
 				tempList.push({ id, ...plans[id] });
@@ -20,12 +20,40 @@ export default function PlanList() {
 			setPlanList(tempList);
 		});
 	}, []);
-	console.log(planList);
+
+	const plans = planList
+		.filter((plan) => {
+			if (query === '') {
+				return plan;
+			} else {
+				return plan.catagory === query;
+			}
+		})
+		.map((plan) => {
+			return <Plan plan={plan} key={plan.id} />;
+		});
+
 	return (
-		<Container className="mt-5 PlanList">
-			{planList
-				? planList.map((plan) => <Plan plan={plan} key={plan.id} />)
-				: ''}
+		<Container className="mt-5">
+			<Form className="col-sm-3 ml-auto d-flex align-items-center">
+				<Form.Label className="ml-auto" style={{ fontSize: '12px' }}>
+					Filter by:
+				</Form.Label>
+				<Form.Control
+					className="col-sm-6 ml-auto"
+					as="select"
+					size="sm"
+					onChange={(e) => setQuery(e.target.value)}
+				>
+					<option value="">unselect</option>
+					<option value="Health">Health</option>
+					<option value="Romance">Romance</option>
+					<option value="Career">Career</option>
+					<option value="Financial">Financial</option>
+					<option value="Recreation">Recreation</option>
+				</Form.Control>
+			</Form>
+			<div className=" PlanList">{plans}</div>
 		</Container>
 	);
 }
