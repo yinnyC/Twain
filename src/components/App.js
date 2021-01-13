6ee4851adc0e2d908b6677d8f8e201573dee5de8
Yin <yinnnn17@gmail.com>
2021-01-12 23:14:29 -0800

    fix routing issue

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AuthProvider } from '../contexts/AuthContext';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import SignupPage from '../pages/SignupPage';
 import Homepage from '../pages/Homepage';
 import PrivateRoute from './PrivateRoute';
